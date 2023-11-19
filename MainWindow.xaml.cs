37cb3e5a41cbeb02ff75cba01a63cf5f530699f1
Kolbert Márió <h241947@stud.u-szeged.hu>
2023-11-19 22:22:12 +0100

    Import files to list and we can delete with contextmenu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,10 +25,27 @@ namespace Szakdolgozat
         public MainWindow()
         {
             InitializeComponent();
+            //filesListing.ContextMenu = CreateContextMenu();
         }
+
+        private List<ImportedFile> selectedFiles = new List<ImportedFile>();
+        /*
+        private ContextMenu CreateContextMenu()
+        {
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem deleteMenuItem = new MenuItem();
+            deleteMenuItem.Header = "Delete";
+            deleteMenuItem.Click += DeleteMenuItem_Click;
+            contextMenu.Items.Add(deleteMenuItem);
+
+            return contextMenu;
+        }*/
 
         private void ListFiles()
         {
+            /*
+             * 0. próbálkozás
+             * Képlistázós dolog
             string path = "../../Assets/";
             string[] files = Directory.GetFiles(path, "*.jpg");
             
@@ -43,11 +61,57 @@ namespace Szakdolgozat
             else
             {
                 tabControlBorder.BorderBrush = Brushes.Green;
+            }*/
+
+            /*
+             * 1. próbálkozás
+
+            filesListing.Items.Clear();
+            
+            foreach (var file in files)
+            {
+                filesListing.Items.Add(System.IO.Path.GetFileName(file));
             }
+
+            if (filesListing.Items.Count == 0)
+            {
+                tabControlBorder.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                tabControlBorder.BorderBrush = Brushes.Green;
+            }*/
+
+            filesListing.ItemsSource = null;
+
+            filesListing.Items.Clear();
+
+            /*
+            foreach (string file in selectedFiles)
+            {
+                filesListing.Items.Add(System.IO.Path.GetFileName(file));
+            }*/
+
+            foreach (ImportedFile importedFile in selectedFiles)
+            {
+                filesListing.Items.Add(importedFile.FileName);
+            }
+
+            if (filesListing.Items.Count == 0)
+            {
+                tabControlBorder.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                tabControlBorder.BorderBrush = Brushes.Green;
+            }
+
         }
 
         private void filesListing_Loaded(object sender, RoutedEventArgs e)
-        {
+        {/*
+          * 0. próbálkozás
+          * Képlistázós dolog, plusz a kör hozzá 
             ListFiles();
 
             List<UIElement> modifiedItems = new List<UIElement>();
@@ -78,6 +142,77 @@ namespace Szakdolgozat
             foreach (var item in modifiedItems)
             {
                 filesListing.Items.Add(item);
+            }*/
+
+            /*
+             * 1. próbálkozás
+            List<UIElement> modifiedItems = new List<UIElement>();
+
+            Random random = new Random();
+            foreach (var item in filesListing.Items)
+            {
+                TextBlock textBlock = new TextBlock() { Text = item.ToString()};
+                modifiedItems.Add(textBlock);
+            }
+
+            filesListing.Items.Clear();
+
+            foreach (var item in modifiedItems)
+            {
+                filesListing.Items.Add(item);
+            }*/
+
+            ListFiles();
+        }
+
+        /// <summary>
+        /// Import excel button click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void importExcel_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string excelFilePath = openFileDialog.FileName;
+                string fileName = System.IO.Path.GetFileName(excelFilePath);
+
+                ImportedFile importedFile = new ImportedFile
+                {
+                    FileName = fileName,
+                    FilePath = excelFilePath
+                };
+
+                selectedFiles.Add(importedFile);
+
+                ListFiles();
+            }
+        }
+
+        private void DeleteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (filesListing.SelectedItem != null) 
+            {
+                string selectedFileName = filesListing.SelectedItem.ToString();
+
+                ImportedFile selectedImportedFile = selectedFiles.FirstOrDefault(file => file.FileName == selectedFileName);
+
+                if (selectedImportedFile != null)
+                {
+                    selectedFiles.Remove(selectedImportedFile);
+                    ListFiles();
+                }
+            }
+        }
+
+        private void filesListing_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            if (filesListing.SelectedItem == null)
+            {
+                e.Handled = true;
             }
         }
     }
