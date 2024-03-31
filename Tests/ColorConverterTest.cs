3da7052e20e193c07b6098946c7499f4f0104679
Kolbert Márió <h241947@stud.u-szeged.hu>
2024-03-31 14:10:02 +0200

    Create NUnit test for first try.

---
+++ Tests/ColorConverterTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Legacy;
+using Szakdolgozat.Converters;
+
+namespace Szakdolgozat.Tests
+{
+    [TestFixture]
+    internal class ColorConverterTest
+    {
+        private ColorConverter _converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _converter = new ColorConverter();
+        }
+
+        [Test]
+        public void ColorTest()
+        {
+            string stringColorName = "Black";
+            string expectedResult = "#000000";
+            object colorName = (object)stringColorName;
+            string result = _converter.ColorNameToHex(colorName);
+
+            ClassicAssert.AreEqual(expectedResult, result);
+        }
+    }
+}
